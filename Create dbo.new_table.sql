5fb15badba1bb2f88ae1991d50c2a23363f2e4d6
saksham123 <sakshmrana@gmail.com>
2019-02-26 21:29:08 -0500

    Add files via upload

---
+++ Create dbo.new_table.sql
@@ -0,0 +1,16 @@
+﻿USE [New Database]
+GO
+
+/****** Object: Table [dbo].[new table] Script Date: 2/26/2019 9:23:56 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[new table] (
+    [Product] NVARCHAR (50) NOT NULL,
+    [Sales]   INT           NULL
+);
+
+
---
+++ Create dbo.old_table.sql
@@ -0,0 +1,16 @@
+﻿USE [New Database]
+GO
+
+/****** Object: Table [dbo].[old table] Script Date: 2/26/2019 9:26:00 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[old table] (
+    [Product] NVARCHAR (50) NOT NULL,
+    [Ranking] INT           NULL
+);
+
+
